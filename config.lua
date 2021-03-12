fdff956d156a4156b31ef9a0c45a0f0ebc92fecd
shagu <ericmauser@googlemail.com>
2021-03-12 19:04:04 +0100

    config: disable optionals by default

--- config.lua
+++ config.lua
@@ -2,12 +2,12 @@ staticmod_config = {
   -- Set to "true" to enable class colors on player and target frame. The
   -- name background that is by default used to display reaction colors such as
   -- hostile, friendly and neutral will be used to display class colors.
-  classcolor = true,
+  classcolor = nil,
 
   -- Set to "true" to enable automatic texture detection. This will scan for
   -- all frames and their textures in order to darken them via SetVertexColor
   -- see darken.lua for details. (incomplete)
-  darken = true,
+  darken = nil,
 
   -- Set to "true" to enable a squared minimap. The original border will be
   -- replaced with the tooltip border that has colors as close as possible to
