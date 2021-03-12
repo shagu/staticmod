30d2941c43242c8511c2a4969aadf3ae81345bd8
shagu <ericmauser@googlemail.com>
2021-03-12 22:41:40 +0100

    tooltip: add proper offsets to backdrop

--- tooltip.lua
+++ tooltip.lua
@@ -1,8 +1,20 @@
+-- set tooltip backdrop
+GameTooltip:SetBackdrop({
+  bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+  edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+  tile = true, tileSize = 8, edgeSize = 16,
+  insets = { left = 3, right = 3, top = 3, bottom = 3 }
+})
+GameTooltip:SetBackdropBorderColor(TOOLTIP_DEFAULT_COLOR.r, TOOLTIP_DEFAULT_COLOR.g, TOOLTIP_DEFAULT_COLOR.b)
+GameTooltip:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BACKGROUND_COLOR.g, TOOLTIP_DEFAULT_BACKGROUND_COLOR.b)
+
+-- resize and move statusbar to bottom
 GameTooltipStatusBar:ClearAllPoints()
 GameTooltipStatusBar:SetPoint("BOTTOMLEFT", 7, 4)
 GameTooltipStatusBar:SetPoint("BOTTOMRIGHT", -7, 4)
 GameTooltipStatusBar:SetHeight(3)
 
+-- change position to match the right chat
 local position = CreateFrame("Frame", nil, GameTooltip)
 position:SetScript("OnShow", function()
   if GameTooltip:GetAnchorType() == "ANCHOR_NONE" then
