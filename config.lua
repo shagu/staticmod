7415afdcbbaeb4d120d9214bd036878821fb0c13
shagu <ericmauser@googlemail.com>
2021-03-12 19:02:52 +0100

    config: update all config values

--- config.lua
+++ config.lua
@@ -1,2 +1,16 @@
 staticmod_config = {
+  -- Set to "true" to enable class colors on player and target frame. The
+  -- name background that is by default used to display reaction colors such as
+  -- hostile, friendly and neutral will be used to display class colors.
+  classcolor = true,
+
+  -- Set to "true" to enable automatic texture detection. This will scan for
+  -- all frames and their textures in order to darken them via SetVertexColor
+  -- see darken.lua for details. (incomplete)
+  darken = true,
+
+  -- Set to "true" to enable a squared minimap. The original border will be
+  -- replaced with the tooltip border that has colors as close as possible to
+  -- the original round texture.
+  minimapsquare = nil,
 }
